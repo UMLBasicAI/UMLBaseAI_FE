3d634ce54732df39f86e11b41bd61821c55ec120
From_VuVuDeveloper <vuvo070403@gmail.com>
2025-04-09 19:29:52 +0700

    [VuVV2]-Fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,10 @@ module.exports = {
         name: "nextjs-app",
         script: "node_modules/next/dist/bin/next",
         args: "start -p 3000",
-        cwd: "/home/azureuser/nextjs-app",
+        cwd: "/home/vuvudev/apps/umlbasicai-fe/production",
         env: {
           NODE_ENV: "production"
         }
       }
     ]
   };
-      
