1f29c4dd338739fc3876f3e0007276c3b90443cd
Vu Vo Developer <vuvo070403@gmail.com>
2025-04-11 18:21:43 +0700

    [VuVV2]-Update Ecosytem Config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: "nextjs-app",
-        script: "node_modules/next/dist/bin/next",
-        args: "start -p 3000",
-        // cwd: "/home/adminplantuml/apps/umlbasicai-fe/production",
-        env: {
-          NODE_ENV: "production"
-        }
+  apps: [
+    {
+      name: "nextjs-app",
+      script: "npx",
+      args: "next start -p 3000",
+      cwd: "/home/adminplantuml/apps/umlbasicai-fe/production",
+      env: {
+        NODE_ENV: "production"
       }
-    ]
-  };
+    }
+  ]
+};
