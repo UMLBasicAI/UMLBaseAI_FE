dc999dd9b0affde090b1525ad682f577eaef89f0
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2025-04-17 02:57:21 +0700

    [QuocNMH] Mapping API get message of a history

--- src/store/feature/ai/ai.ts
+++ src/store/feature/ai/ai.ts
@@ -1,9 +1,23 @@
 export interface PromptToAIResponse {
-    appCode: string;
-    body: {
-      historyId: string;
-      plantUML: string;
-      responseText: string;
-    };
-  }
-  
+  appCode: string;
+  body: {
+    historyId: string;
+    plantUML: string;
+    responseText: string;
+  };
+}
+
+export interface HistoryResponse {
+  appCode: string;
+  body: {
+    historyId: string;
+    messages: Array<{
+      content: string;
+      messageType: string;
+      createdAt: string;
+      updatedAt: string;
+    }>;
+    isHasNextPage: boolean;
+    isHasPreviousPage: boolean;
+  };
+}
